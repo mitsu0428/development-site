1322182f2723264aae9b34068fa7d77ca90e9fb5
mitsu0428 <mitsuprograming@gmail.com>
2023-01-21 22:26:23 +0900

    最初のページを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
 function HomePage() {
-  return <div>Welcome to tetete</div>;
+    return (
+        <Container>
+            <DefaultTitle>Welcome to tetete</DefaultTitle>
+            <DefaultText>
+                これから最高のページになっていくよ!ててて!
+            </DefaultText>
+        </Container>
+    )
 }
 
-export default HomePage;
+export default HomePage
+
+const Container = styled.div`
+    width: 100%;
+`
+
+const DefaultTitle = styled.h1`
+    font-size: 64px;
+`
+
+const DefaultText = styled.p`
+    font-size: 32px;
+`
